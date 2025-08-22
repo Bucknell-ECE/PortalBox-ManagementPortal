0bdeb589fd66520d379d155ebdd73e05107e5c40
Tom Egan <tom@tomegan.tech>
2025-08-22 14:18:39 -0400

    cleanup: delete dead code

--- public/api/box.php
+++ public/api/box.php
@@ -12,7 +12,6 @@ use Portalbox\Model\LocationModel;
 use Portalbox\Exception\DatabaseException;
 use Portalbox\Query\EquipmentQuery;
 use Portalbox\Transform\EquipmentTransformer;
-use Portalbox\Model\CardModel;
 use Portalbox\Query\CardQuery;
 use Portalbox\Session\Session;
 use Portalbox\Transform\CardTransformer;
