f6d0765eb2474810759de0a96ff612d1c06b2121
Tom Egan <tom@tomegan.tech>
2026-03-19 11:26:52 -0400

    chore: refactor reading usage log to service code

--- src/Service/LoggedEventService.php
+++ src/Service/LoggedEventService.php
@@ -6,6 +6,7 @@ namespace Portalbox\Service;
 
 use DateInterval;
 use DateTimeImmutable;
+use InvalidArgumentException;
 use Portalbox\Enumeration\Permission;
 use Portalbox\Exception\AuthenticationException;
 use Portalbox\Exception\AuthorizationException;
@@ -19,6 +20,14 @@ use Portalbox\Session;
 class LoggedEventService {
 	public const ERROR_UNAUTHENTICATED = 'You must be authenticated to read the requested statistics';
 	public const ERROR_UNAUTHORIZED_READ_OF_STATISTICS = 'You are not authorized to read the requested statistics';
+	public const ERROR_UNAUTHENTICATED_READ = 'You must be authenticated to read the usage log';
+	public const ERROR_UNAUTHORIZED_READ = 'You are not authorized to read the usage log';
+
+	public const ERROR_EQUIPMENT_FILTER_MUST_BE_INT = 'The equipment filter must be the integer id of equipment';
+	public const ERROR_EQUIPMENT_TYPE_FILTER_MUST_BE_INT = 'The equipment type filter must be the integer id of an equipment type';
+	public const ERROR_LOCATION_FILTER_MUST_BE_INT = 'The location filter must be the integer id of a location';
+	public const ERROR_BEFORE_FILTER_MUST_BE_DATE = 'The before filter must be a date';
+	public const ERROR_AFTER_FILTER_MUST_BE_DATE = 'The after filter must be a date';
 
 	protected Session $session;
 	protected LoggedEventModel $loggedEventModel;
@@ -139,5 +148,72 @@ class LoggedEventService {
 		}
 
 		return $paddedCounts;
+	}
+
+	/**
+	 * Read log entries passing the filters
+	 *
+	 * @param array<string, string>  filters that all log entries in the result
+	 *      set must meet
+	 * @return LoggedEvent[]  the log entries passing the filters
+	 * @throws AuthenticationException  if no user is authenticated
+	 * @throws AuthorizationException  if the authenticated user may not read
+	 *      the log
+	 * @throws InvalidArgumentException if the equipment, equipment type or
+	 *      location filters specify a value that can not be understood as an
+	 *      integer
+	 * @throws Exception (PHP <= 8.2) or DateMalformedStringException (PHP > 8.2)
+	 *      if the before or after filters can not be understood as dates
+	 */
+	public function readAll(array $filters): array {
+		$authenticatedUser = $this->session->get_authenticated_user();
+		if ($authenticatedUser === null) {
+			throw new AuthenticationException(self::ERROR_UNAUTHENTICATED_READ);
+		}
+
+		if (!$authenticatedUser->role()->has_permission(Permission::LIST_LOGS)) {
+			throw new AuthorizationException(self::ERROR_UNAUTHORIZED_READ);
+		}
+
+		$query = new LoggedEventQuery();
+
+		if(isset($filters['equipment_id']) && !empty($filters['equipment_id'])) {
+			$equipment_id = filter_var($filters['equipment_id'], FILTER_VALIDATE_INT);
+			if ($equipment_id === false) {
+				throw new InvalidArgumentException(self::ERROR_EQUIPMENT_FILTER_MUST_BE_INT);
+			}
+
+			$query->set_equipment_id($equipment_id);
+		}
+
+		if(isset($filters['equipment_type_id']) && !empty($filters['equipment_type_id'])) {
+			$equipment_type_id = filter_var($filters['equipment_type_id'], FILTER_VALIDATE_INT);
+			if ($equipment_type_id === false) {
+				throw new InvalidArgumentException(self::ERROR_EQUIPMENT_TYPE_FILTER_MUST_BE_INT);
+			}
+
+			$query->set_equipment_type_id($equipment_type_id);
+		}
+
+		if(isset($filters['location_id']) && !empty($filters['location_id'])) {
+			$location_id = filter_var($filters['location_id'], FILTER_VALIDATE_INT);
+			if ($location_id === false) {
+				throw new InvalidArgumentException(self::ERROR_LOCATION_FILTER_MUST_BE_INT);
+			}
+
+			$query->set_location_id($location_id);
+		}
+
+		if(isset($filters['after']) && !empty($filters['after'])) {
+			$after = new DateTimeImmutable($filters['after']);
+			$query->set_on_or_after($after->format('Y-m-d'));
+		}
+
+		if(isset($filters['before']) && !empty($filters['before'])) {
+			$before = new DateTimeImmutable($filters['before']);
+			$query->set_on_or_before($before->format('Y-m-d'));
+		}
+
+		return $this->loggedEventModel->search($query);
 	}
 }
